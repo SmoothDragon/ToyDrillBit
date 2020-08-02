4ae3a9c8a1cc0e50bf9b3d3bbc523ef2df58598a
Thomas Draper <tom@betterservers.com>
2020-08-02 00:16:44 -0700

    Bevel end of bit and modify dimensions according to feedback.

--- ToyDrillBit.scad.py
+++ ToyDrillBit.scad.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python3
 
 '''
-Toy drill bit for "jThink Gizmos take apart racing car".
+Toy drill bit for "Think Gizmos take apart racing car".
 '''
 
 import numpy as np
 
-from solid import *
-from solid.utils import *
 import solid
 
 
-def bit(hex_dia, hex_depth, drive_fin_dia, drive_fin_width):
+def bit(hex_dia, hex_depth, drive_fin_dia, drive_fin_width, bevel=1):
     block = solid.cylinder(d=hex_dia*2/np.sqrt(3), h=hex_depth, segments=6)
     taper = solid.cylinder(d1=hex_dia*2/np.sqrt(3), d2=0, h=hex_depth*3/2, segments=6)
     taper = solid.translate([0,0,hex_depth])(taper)
@@ -20,7 +18,9 @@ def bit(hex_dia, hex_depth, drive_fin_dia, drive_fin_width):
     fin = solid.cube([drive_fin_dia, drive_fin_width, 3*hex_depth], center=True)
     # block += solid.translate([0,0,hex_depth])(
         # solid.cylinder(d1=hex_dia*2/np.sqrt(3), d2=drive_fin_dia, h=hex_depth, segments=6))
-    fin = solid.cube([drive_fin_dia, drive_fin_width, 3*hex_depth], center=True)
+    fin = solid.cube([drive_fin_dia-2*bevel, drive_fin_width, 3*hex_depth], center=True)
+    fin += solid.cube([drive_fin_dia, drive_fin_width, 3*hex_depth-2*bevel], center=True)
+    fin = solid.hull()(fin)
     fin = solid.translate([0,0,1.5*hex_depth])(fin)
     fin += solid.rotate([0,0,90])(fin)
     block += fin
@@ -28,9 +28,9 @@ def bit(hex_dia, hex_depth, drive_fin_dia, drive_fin_width):
 
 
 if __name__ == '__main__':
-    hex_dia = 25.4*5/8
+    hex_dia = 14  # 25.4*9/16=14.2875
     hex_depth = 25.4/2
-    drive_fin_dia = 25.4*3/8
-    drive_fin_width = 25.4/16
+    drive_fin_dia = 9.5  # 25.4*3/8=9.525
+    drive_fin_width = 1.5  # 25.4/16=1.5875
     final = bit(hex_dia, hex_depth, drive_fin_dia, drive_fin_width)
-    print(scad_render(final))
+    print(solid.scad_render(final))
